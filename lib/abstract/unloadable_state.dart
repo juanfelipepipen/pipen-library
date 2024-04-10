720d51c939ed4e3834cb9872c07d4705ca454a84
Juan Felipe Lopez Estrada <juanfelipepipen@gmail.com>
2024-04-09 21:30:59 -0700

    fix: minimal corrections

--- lib/abstract/unloadable_state.dart
+++ lib/abstract/unloadable_state.dart
@@ -1,1 +1,6 @@
-abstract class UnloadableState {}
+import 'package:pipen/models/fail_result.dart';
+
+abstract class UnloadableState {
+  /// Event emitted, example: exception throw
+  FailResult get fail;
+}
