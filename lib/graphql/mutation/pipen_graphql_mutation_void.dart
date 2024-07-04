2fa918980f4bba1fd9c4bb15451309fc3322368a
Juan Felipe Lopez Estrada <juanfelipepipen@gmail.com>
2024-07-03 20:46:33 -0700

    fix: void generic on void mutation-query

--- lib/graphql/mutation/pipen_graphql_mutation_void.dart
+++ lib/graphql/mutation/pipen_graphql_mutation_void.dart
@@ -1,6 +1,6 @@
 import 'package:pipen/graphql/mutation/pipen_graphql_mutation.dart';
 
-abstract class PipenGraphqlMutationVoid extends PipenGraphqlMutation {
+abstract class PipenGraphqlMutationVoid extends PipenGraphqlMutation<void> {
   @override
   get onSuccess => (params) async {};
 }
--- lib/graphql/query/pipen_graphql_query_void.dart
+++ lib/graphql/query/pipen_graphql_query_void.dart
@@ -1,6 +1,6 @@
 import 'package:pipen/graphql/query/pipen_graphql_query.dart';
 
-abstract class PipenGraphqlQueryVoid extends PipenGraphqlQuery {
+abstract class PipenGraphqlQueryVoid extends PipenGraphqlQuery<void> {
   @override
   get onSuccess => (params) async {};
 }
