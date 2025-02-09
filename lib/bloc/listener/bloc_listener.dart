154e33f2c388e268e8fcb31247a415120457560a
Juan Felipe Lopez Estrada <juanfelipepipen@gmail.com>
2025-02-08 20:48:41 -0700

    fix: bloc listener correction

--- lib/bloc/listener/bloc_listener.dart
+++ lib/bloc/listener/bloc_listener.dart
@@ -1,18 +1,18 @@
-import 'package:flutter_bloc/flutter_bloc.dart' as bloc;
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pipen/bloc/builder/bloc_builder_p.dart';
 import 'package:flutter/widgets.dart';
 
-class BlocListenerP<B extends bloc.StateStreamable<S>, S> extends StatelessWidget {
+class BlocListenerP<B extends StateStreamable<S>, S> extends StatelessWidget {
   BlocListenerP({this.builder, this.child, required this.listener, super.key}) {
     assert(builder != null || child != null);
   }
 
   final Widget Function(BuildContext context, S state, B bloc)? builder;
-  final bloc.BlocWidgetListener<S> listener;
+  final BlocWidgetListener<S> listener;
   final Widget? child;
 
   @override
-  Widget build(BuildContext context) => BlocListenerP<B, S>(
+  Widget build(BuildContext context) => BlocListener<B, S>(
         listener: listener,
         child: child ??
             BlocBuilderP<B, S>(
