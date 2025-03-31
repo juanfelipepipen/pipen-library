8561f3e46bcb2dbcab18dc1a5263f9229bd56b63
Juan Felipe Lopez Estrada <juanfelipepipen@gmail.com>
2025-03-31 16:26:47 -0700

    fix: min size implementation correction on PipenBuilderWhen

--- lib/components/builders/builder/pipen_builder_when.dart
+++ lib/components/builders/builder/pipen_builder_when.dart
@@ -13,15 +13,16 @@ class PipenBuilderWhen<W> extends StatelessWidget {
     this.width,
     this.margin,
     this.skeleton,
+    this.minHeight,
     this.alignment,
     this.loading = false,
     required this.value,
     required this.child,
   });
 
+  final double? height, width, minHeight;
   final Widget Function(W on) child;
   final Widget? skeleton, deny;
-  final double? height, width;
   final Alignment? alignment;
   final EdgeInsets? margin;
   final dynamic value;
@@ -31,18 +32,20 @@ class PipenBuilderWhen<W> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Container(
-        width: width,
-        height: height,
-        alignment: alignment,
-        margin: margin ?? EdgeInsets.zero,
-        child: skeleton != null
+    width: width,
+    alignment: alignment,
+    height: minHeight == null ? height : null,
+    constraints: height == null && minHeight != null ? BoxConstraints(minHeight: minHeight!) : null,
+    margin: margin ?? EdgeInsets.zero,
+    child:
+        skeleton != null
             ? Skeleton(
-                skeleton: skeleton!,
-                isLoading: !buildNow,
-                child: buildNow ? child.call(value as W) : const SizedBox.shrink(),
-              )
+              skeleton: skeleton!,
+              isLoading: !buildNow,
+              child: buildNow ? child.call(value as W) : const SizedBox.shrink(),
+            )
             : buildNow
-                ? child.call(value as W)
-                : deny ?? const SizedBox.shrink(),
-      );
+            ? child.call(value as W)
+            : deny ?? const SizedBox.shrink(),
+  );
 }
